5c641fa66c06510c758b31ff205891d874740421
WilliamAalund <williamaalund@icloud.com>
2024-01-27 16:38:26 -0600

    Altered python script to generate a JSON

--- YFinanceStuff.py
+++ YFinanceStuff.py
@@ -1,13 +1,23 @@
+# pip install yfinance
+
 import yfinance as yf
 import datetime
 import pandas as pd
 import csv
+import json
 
 ticker = "META"
 startDate = datetime.datetime(2018, 1, 1)
 endDate = datetime.datetime(2018,3, 1)
 tickerInformation = yf.Ticker(ticker)
 tickerHistoryInformation = tickerInformation.history(start = startDate, end = endDate)
+
+# Convert the dictionary to a JSON string
+ticker_json = tickerHistoryInformation.to_json()
+
+# Write the JSON string to a file
+with open('tickerInformation.json', 'w') as f:
+    f.write(ticker_json)
 
 tickerHistoryInformation.to_csv("tickertag{}_history.csv")
 #print(GetInformation.history(start = startDate, end = endDate))
