967b679afce511190bb68d22df86948a36a905ab
Isaac Geng <isaacgeng2004@gmail.com>
2024-01-27 17:02:32 -0600

    made function to load ticker

--- YFinanceStuff.py
+++ YFinanceStuff.py
@@ -3,11 +3,25 @@ import datetime
 import pandas as pd
 import csv
 
-ticker = "META"
-startDate = datetime.datetime(2018, 1, 1)
-endDate = datetime.datetime(2018,3, 1)
-tickerInformation = yf.Ticker(ticker)
-tickerHistoryInformation = tickerInformation.history(start = startDate, end = endDate)
+Q1 = datetime.datetime()
 
-tickerHistoryInformation.to_csv("tickertag{}_history.csv")
+
+def loadTickerJSON(tickerName, quarter, year):
+    if(quarter == 1):
+        startDate = datetime.datetime(year, 1, 1)
+        endDate = datetime.datetime(year,3, 31)
+    elif(quarter == 2):
+        startDate = datetime.datetime(year, 4, 1)
+        endDate = datetime.datetime(year,6, 31)   
+    elif(quarter == 3):
+        startDate = datetime.datetime(year, 7, 1)
+        endDate = datetime.datetime(year,9, 31)   
+    elif(quarter == 4):
+        startDate = datetime.datetime(year, 10, 1)
+        endDate = datetime.datetime(year,12, 31)  
+
+    tickerInformation = yf.Ticker(ticker)
+    tickerHistoryInformation = tickerInformation.history(start = startDate, end = endDate)
+
+    #tickerHistoryInformation.to_csv("tickertag{}_history.csv")
 #print(GetInformation.history(start = startDate, end = endDate))
