f79efcb0931ca3f64173773b1d270707acab3b0f
WilliamAalund <williamaalund@icloud.com>
2024-01-27 17:07:08 -0600

    Refined merge

--- YFinanceStuff.py
+++ YFinanceStuff.py
@@ -6,8 +6,6 @@ import pandas as pd
 import csv
 import json
 
-Q1 = datetime.datetime()
-
 
 def loadTickerJSON(tickerName, quarter, year):
     if(quarter == 1):
@@ -23,15 +21,12 @@ def loadTickerJSON(tickerName, quarter, year):
         startDate = datetime.datetime(year, 10, 1)
         endDate = datetime.datetime(year,12, 31)  
 
-    tickerInformation = yf.Ticker(ticker)
+    tickerInformation = yf.Ticker(tickerName)
     tickerHistoryInformation = tickerInformation.history(start = startDate, end = endDate)
+    # Convert the dictionary to a JSON string
+    ticker_json = tickerHistoryInformation.to_json()
 
-    #tickerHistoryInformation.to_csv("tickertag{}_history.csv")
-# Convert the dictionary to a JSON string
-ticker_json = tickerHistoryInformation.to_json()
+    # Write the JSON string to a file
+    with open('tickerInformation.json', 'w') as f:
+        f.write(ticker_json)
 
-# Write the JSON string to a file
-with open('tickerInformation.json', 'w') as f:
-    f.write(ticker_json)
-
-#print(GetInformation.history(start = startDate, end = endDate))
