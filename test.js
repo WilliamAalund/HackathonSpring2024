d31b2093fd7ebaf858a5788613ae0e3c3720f4f4
CowTippingFool <harrison@melissaandjeff.com>
2024-01-28 01:07:21 -0600

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,18 @@ var selected
 var buttons = document.getElementsByClassName("list-group");
 var previous = ''
 
+function resetButtons() {
+    for (i = 0; i < buttons.length; i++) {
+        if(buttons[i].classList.contains('active')) {
+            buttons[i].classList.toggle('active');
+        }
+    }
+}
+
 function toggleEnable(name) {
     selected = name;
+
+    resetButtons()
 
     document.getElementById(selected).classList.toggle('active');
     if (!(previous == name || previous == '')) {
